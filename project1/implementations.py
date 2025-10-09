315a8fce7dfc3c20116134a1f788535a2554dcd1
jangv04 <jangambusvinyeta@gmail.com>
2025-10-09 13:20:27 +0200

    Update implementations.py

--- project1/implementations.py
+++ project1/implementations.py
@@ -1,9 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Loss Functions
+
 def mse_loss(y, tx, w):
     error = y - tx @ w
     return np.mean(error ** 2) / 2
+
+def sigmoid(t):
+    return 1 / (np.exp(-t)+1)
+
+def logistic_loss(y, tx, w):
+    pred = sigmoid(tx @ w)                    
+    loss = -np.mean(y * np.log(pred) + (1 - y) * np.log(1 - pred))
+    return float(loss)
+
+#Compute Gradients
 
 def compute_gradient_mse(y, tx, w):
     N = y.shape[0]
@@ -13,14 +25,22 @@ def compute_gradient_mse(y, tx, w):
 def compute_gradient_stochastic(y_i, x_i, w):
     error = y_i - x_i @ w        
     return -error * x_i 
+
+def compute_gradient_logistic(y, tx, w):
+    pred = sigmoid(tx @ w)               
+    grad = tx.T @ (pred - y) / y.shape[0]
+    return grad
     
+
+#Gradient Descent
+
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     ws = [initial_w]
     losses = []
     w = initial_w
     
     for n_iter in range(max_iters):
-        gradient = compute_gradient(y, tx, w)    
+        gradient = compute_gradient_mse(y, tx, w)    
         loss = mse_loss(y, tx, w)
         
         w = w - gamma * gradient
@@ -49,6 +69,73 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
         losses.append(loss)
         
     return ws[-1], loss[-1]
+
+def learning_by_gradient_descent_logistic(y, tx, w, gamma):
+    grad = compute_gradient_logistic(y, tx, w)
+    loss = logistic_loss(y, tx, w)
+    w = w - gamma * grad
+
+    return loss, w
+
+def logistic_regression_gradient_descent(y, x):
+    # init parameters
+    max_iter = 10000
+    threshold = 1e-8
+    gamma = 0.5
+    losses = []
+
+    # build tx
+    tx = np.c_[np.ones((y.shape[0], 1)), x]
+    w = np.zeros((tx.shape[1], 1))
+
+    # start the logistic regression
+    for iter in range(max_iter):
+        # get loss and update w.
+        loss, w = learning_by_gradient_descent_logistic(y, tx, w, gamma)
+        # log info
+        if iter % 100 == 0:
+            print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
+        # converge criterion
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            break
+          
+def penalized_logistic_regression(y, tx, w, lambda_):
+    loss = logistic_loss(y, tx, w)
+    grad = compute_gradient_logistic(y, tx, w)
+    grad += 2* lambda_ * w  
+    return loss, grad
+
+def learning_by_penalized_gradient(y, tx, w, gamma, lambda_):
+
+    loss, grad = penalized_logistic_regression(y, tx, w, lambda_)
+    w = w - gamma * grad
+    
+def logistic_regression_penalized_gradient_descent_demo(y, x):
+    # init parameters
+    max_iter = 10000
+    gamma = 0.5
+    lambda_ = 0.0005
+    threshold = 1e-8
+    losses = []
+
+    # build tx
+    tx = np.c_[np.ones((y.shape[0], 1)), x]
+    w = np.zeros((tx.shape[1], 1))
+
+    # start the logistic regression
+    for iter in range(max_iter):
+        # get loss and update w.
+        loss, w = learning_by_penalized_gradient(y, tx, w, gamma, lambda_)
+        # log info
+        if iter % 100 == 0:
+            print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
+        # converge criterion
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            break
+
+#Explicit Solutions
         
 def least_squares(y, tx):
     A = tx.T @ tx
@@ -56,7 +143,6 @@ def least_squares(y, tx):
     w = np.linalg.solve(A, b)
     mse = mse_loss(y, tx, w)
     return w, mse
-
 
 def ridge_regression(y, tx, lambda_):
     D = tx.shape[1]
@@ -69,8 +155,4 @@ def ridge_regression(y, tx, lambda_):
     rmse = np.sqrt(2 * mse_loss(y, tx, w))
     return w, rmse
 
-     
-# def logistic_regression(y, tx, initial_w, max_iters, gamma):
-    
-    
-# def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+
